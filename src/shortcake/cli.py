197270d236f3be450db4742862699345de3a25ee
Patrick Arminio <patrick.arminio@gmail.com>
2026-01-22 00:33:31 +0000

    feat: implement `sc log` command (#39)
    Show commits on current branch between parent branch and HEAD
    with a tree-style output using pipes:
    
    ◉ feature
    │
    ● abc1234 First commit
    │
    ● def5678 Second commit
    │
    ◯ main
    
    Uses Shortcake-Parent trailer to determine parent, falls back
    to default branch for untracked branches.

--- src/shortcake/cli.py
+++ src/shortcake/cli.py
@@ -4,6 +4,7 @@ from shortcake.commands.adopt import adopt
 from shortcake.commands.bottom import bottom
 from shortcake.commands.create import create
 from shortcake.commands.down import down
+from shortcake.commands.log import log
 from shortcake.commands.ls import ls
 from shortcake.commands.modify import modify
 from shortcake.commands.top import top
@@ -22,6 +23,7 @@ app.command()(adopt)
 app.command()(bottom)
 app.command()(create)
 app.command()(down)
+app.command()(log)
 app.command()(ls)
 app.command()(modify)
 app.command()(top)
