23749b2032a655917d8b3b4c05bf04f9f83f8927
Patrick Arminio <patrick.arminio@gmail.com>
2026-01-27 10:52:02 +0000

    fix: restructure try/except for better coverage pragma

--- src/shortcake/commands/checkout.py
+++ src/shortcake/commands/checkout.py
@@ -43,9 +43,9 @@ def _fetch_branch(repo: Repo, branch: str) -> bool:
             "origin",
             quiet=True,
         )
-        return True
-    except Exception:
-        return False  # pragma: no cover
+    except Exception:  # pragma: no cover
+        return False
+    return True
 
 
 def _create_branch_from_remote(repo: Repo, branch: str) -> bool:
