b4ed7a7f08106e9302ebcf09169fa409aac2d85d
Patrick Arminio <patrick.arminio@gmail.com>
2026-01-27 10:58:57 +0000

    fix: move pragma to correct line for coverage

--- src/shortcake/commands/checkout.py
+++ src/shortcake/commands/checkout.py
@@ -43,9 +43,9 @@ def _fetch_branch(repo: Repo, branch: str) -> bool:
             "origin",
             quiet=True,
         )
-    except Exception:  # pragma: no cover
-        return False
-    return True
+        return True
+    except Exception:
+        return False  # pragma: no cover
 
 
 def _create_branch_from_remote(repo: Repo, branch: str) -> bool:
